2844e4aeeee5eb7b7736f78a4b85854e956cac8f
John Pepp <jrpepp@pepster.com>
2017-02-08 16:20:34 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -32,6 +32,9 @@ January 31, 2017
         <link rel="stylesheet" href="lib/css/stylesheet.css">
     </head>
     <body>
+        <noscript>
+        <p>This page requires JavaScript to function properly.</p>
+        </noscript>
         <div id="container">
             <div id="heading">
                 <h1>Trivia<span id="toxic">IntoXication</span></h1>
@@ -39,37 +42,18 @@ January 31, 2017
             </div>
 
             <div id="mainGame">
-                <div id="screen">
-                    <!-- The Question that is pulled in from PHP and JavaScript -->
-                    <h2 id="question">Question</h2>
-                </div>
-                <!-- The Possible Answers that is also pulled in from PHP and pure JavaScript -->
-                <button id="answer1" class="answers enable" data-answer="1">&nbsp;</button>
-                <button id="answer2" class="answers enable" data-answer="2">&nbsp;</button>
-                <button id="answer3" class="answers enable" data-answer="3">&nbsp;</button>
-                <button id="answer4" class="answers enable" data-answer="4">&nbsp;</button>
-
-                <div id="scoring">
-                    <h2><span id="correct">&#10004;  0</span><span id="wrongText">&#10004; 0</span></h2>
-                </div>
-
-                <div id="display">
-                    <h2 id="clock">&nbsp;</h2>
-                    <button id="next">Next</button>
-                </div>
-
-
             </div>
 
             <div id="info">
                 <p>I have decided to chuck jQuery to the trash bin and convert this game over to pure JavaScript (some call it vanilla JavaScript), so when I eventually deem this HTML/CSS game worthy enough for a tutorial it will be more portable. It should be also easier for people to write javascript, for in my opinion learning javascript makes it easier to understand and there really is not that much more code added.</p>
             </div>
-            
+
             <div id="myFooter">
                 <p class="footer-text">&copy;<?php echo date("Y"); ?> John R. Pepp</p>
             </div>
-            
+
         </div>
+        <script src="lib/js/display.js"></script>
         <script src="lib/js/trivia.js"></script>
     </body>
 </html>
