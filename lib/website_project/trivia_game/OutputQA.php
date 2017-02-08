d8814305b8bf2e202cbff3da935aa5a3ec8d264d
John Pepp <jrpepp@pepster.com>
2017-02-08 16:22:25 -0500

    Add files via upload

--- lib/website_project/trivia_game/OutputQA.php
+++ lib/website_project/trivia_game/OutputQA.php
@@ -10,6 +10,7 @@ use DateTimeZone;
 class OutputQA {
 
     public $result = \NULL;
+    public $data = \NULL;
     protected $pdo = \NULL;
     protected $q_num = \NULL;
     protected $query = \NULL;
@@ -19,7 +20,25 @@ class OutputQA {
     public function __construct() {
         
     }
-    
+
+    public function read($start = 0, $end = 10, $category = "movie") {
+        $db = DB::getInstance();
+        $pdo = $db->getConnection();
+        $this->query = "SELECT id, question, answer1, answer2, answer3, answer4, correct, category, play_date FROM trivia_questions WHERE category=:category ORDER BY id ASC LIMIT :start, :end";
+        $this->stmt = $pdo->prepare($this->query);
+
+        $this->stmt->bindValue(':category', $category, PDO::PARAM_STR);
+        $this->stmt->bindValue(':start', (int) $start, PDO::PARAM_INT);
+        $this->stmt->bindValue(':end', (int) $end, PDO::PARAM_INT);
+        
+
+        $this->stmt->execute();
+
+        $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
+
+        return $this->data;
+    }
+
     public function readQA($q_num) {
         $play_date = new DateTime("Now", new DateTimeZone("America/Detroit"));
         $db = DB::getInstance();
